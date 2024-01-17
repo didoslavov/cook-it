f2a262f380b0bd5ad48ee96117cf116f8e15b877
didoslavov <didkoslawow@gmail.com>
2024-01-17 19:35:12 +0200

    custom error and global error handler middleware

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import { urlencoded, json, Application } from 'express';
 import cors from '../middlewares/cors';
 import cookieParser from 'cookie-parser';
+import errorHandlerMiddleware from '../middlewares/errorHandler';
 
 dotenv.config();
 const PORT: string | number = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ export default (app: Application): void => {
     app.use(json());
     app.use(cors());
     app.use(cookieParser());
+    app.use(errorHandlerMiddleware);
 
     app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
 };

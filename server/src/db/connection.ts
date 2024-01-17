7414ca8754b5940d9cf0ced634400a2a79383a43
didoslavov <didkoslawow@gmail.com>
2024-01-17 15:47:41 +0200

    sequelize configuration

---
+++ server/src/db/connection.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize-typescript';
+
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    models: [__dirname + '/models'],
+});
+
+export default sequelize;

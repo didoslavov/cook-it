992f70cf91aac766752bb24c3117306a45f4606b
didoslavov <didkoslawow@gmail.com>
2024-01-18 03:54:30 +0200

    Shared component

---
+++ server/src/components/Shared/index.ts
@@ -0,0 +1,5 @@
+export { default as AppError } from './errors/appError';
+export { default as cors } from './middlewares/cors';
+export { default as errorHandlerMiddleware } from './middlewares/errorHandler';
+// export * from './utils/bcrypt';
+// export * from './utils/jwt';

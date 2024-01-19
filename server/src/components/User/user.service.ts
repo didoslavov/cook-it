4dd6871013d46c992f08a041d22b566e8b4bdad2
didoslavov <didkoslawow@gmail.com>
2024-01-20 01:43:24 +0200

    added findUserById in user service

--- server/src/components/User/user.service.ts
+++ server/src/components/User/user.service.ts
@@ -1,9 +1,13 @@
 import { User, UserInterface } from '../User';
 
-export const createUser = async (user: UserInterface): Promise<UserInterface | undefined> => {
-    return await User.create(user).then((user): UserInterface | undefined => user.toJSON());
+export const createUser = async (user: UserInterface): Promise<UserInterface> => {
+    return await User.create(user).then((user): UserInterface => user.toJSON());
 };
 
-export const findUser = async (email: string): Promise<UserInterface | undefined> => {
+export const findUserByEmail = async (email: string): Promise<UserInterface | undefined> => {
     return await User.findOne({ where: { email } }).then((user): UserInterface | undefined => user?.toJSON());
+};
+
+export const findUserById = async (id: string): Promise<User | undefined> => {
+    return await User.findByPk(id).then((user): User | undefined => user?.toJSON());
 };

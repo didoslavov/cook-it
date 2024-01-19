e1151fb5f9749933b55a947ecf45a4fd06cfa4fb
didoslavov <didkoslawow@gmail.com>
2024-01-19 04:48:25 +0200

    added jwt helper functions

---
+++ server/src/components/Shared/utils/jwt.ts
@@ -0,0 +1,18 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+const secret = process.env.SECRET || 'Potato';
+
+function createToken(data: string | object): string {
+    return jwt.sign(data, secret, { expiresIn: '1d' });
+}
+
+function verifyToken(token: string): Promise<string | JwtPayload | undefined> {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, data): void => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+}

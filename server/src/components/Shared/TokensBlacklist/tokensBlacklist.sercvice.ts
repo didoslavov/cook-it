6bb5cfd8641b77f8504d27f518a80ce871dbd65d
didoslavov <didkoslawow@gmail.com>
2024-01-20 01:43:01 +0200

    tokens blacklist service added

---
+++ server/src/components/Shared/TokensBlacklist/tokensBlacklist.sercvice.ts
@@ -0,0 +1,12 @@
+import TokensBlacklist from './TokensBlacklist.model';
+import { BlackListed } from './tokensBlacklist.interface';
+
+const blackListToken = async (token: string): Promise<BlackListed> => {
+    return await TokensBlacklist.create({ token }).then((token): BlackListed => token.toJSON());
+};
+
+const getBlacklistedToken = async (token: string): Promise<BlackListed | undefined> => {
+    return await TokensBlacklist.findOne({ where: { token } }).then((token): BlackListed | undefined => token?.toJSON());
+};
+
+export { blackListToken, getBlacklistedToken };

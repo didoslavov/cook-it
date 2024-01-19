4b1e97c12a99929e745f8ca9b364c56ed91a52e4
didoslavov <didkoslawow@gmail.com>
2024-01-20 00:00:14 +0200

    login function added

--- server/src/components/User/auth.controller.ts
+++ server/src/components/User/auth.controller.ts
@@ -1,12 +1,35 @@
 import expressAsyncHandler from 'express-async-handler';
-import { createUser } from '.';
-import { User } from '.';
+import { createUser, findUser, User } from '.';
+import { Request, Response } from 'express';
+import { AppError, comparePasswords, createToken } from '../Shared';
 
-const register = expressAsyncHandler(async (req, res) => {
+const register = expressAsyncHandler(async (req, res): Promise<void> => {
     const userData: User = req.body;
 
     const user = await createUser(userData);
     res.status(200).json(user);
 });
 
-export { register };
+const login = expressAsyncHandler(async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.body;
+
+    const user = await findUser(email);
+
+    if (!user) {
+        throw new AppError(401, "User or password don't match!");
+    }
+
+    const isPasswordMatch = comparePasswords(user.password, password);
+
+    if (!isPasswordMatch) {
+        throw new AppError(401, "User or password don't match!");
+    }
+
+    const token = createToken(user);
+    const userData = { id: user.id, firstName: user.firstName, lastName: user.lastName, email: user.email };
+
+    res.cookie('auth', token, { httpOnly: true, secure: false });
+    res.status(200).json({ user: userData });
+});
+
+export { register, login };
--- server/src/components/User/user.routes.ts
+++ server/src/components/User/user.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { register } from './auth.controller';
+import { register, login } from './auth.controller';
 import authMiddleware from '../Shared/middlewares/authMiddleware';
 const usersRouter = Router();
 
 usersRouter.get('/');
-usersRouter.post('/login');
+usersRouter.post('/login', login);
 usersRouter.post('/register', register);
 usersRouter.post('/logout', authMiddleware);
 usersRouter.patch('/:userId/update');

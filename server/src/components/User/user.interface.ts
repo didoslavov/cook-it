0cea25cd25590a242d47c7f548228fec1d8db8dc
didoslavov <didkoslawow@gmail.com>
2024-01-19 23:59:37 +0200

    added user related interfaces

--- server/src/components/User/user.interface.ts
+++ server/src/components/User/user.interface.ts
@@ -1,6 +1,20 @@
+import { Request } from 'express';
+
 export interface UserInterface {
+    id: string;
     firstName: string;
     lastName: string;
     email: string;
     password: string;
+}
+
+export interface UserRequest extends Request {
+    user?: {};
+}
+
+export interface UserDataRequest extends Request {
+    user: {
+        email: string;
+        password: string;
+    };
 }

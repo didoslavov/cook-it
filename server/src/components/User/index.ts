5401a855354be3e100b902f295f4fb5dbedb67b2
didoslavov <didkoslawow@gmail.com>
2024-01-19 18:39:35 +0200

    using usercontroller in user routes

--- server/src/components/User/index.ts
+++ server/src/components/User/index.ts
@@ -1,6 +1,5 @@
 export { default as User } from './User.model';
 
-export * from './user.interface';
 export { default as userRouter } from './user.routes';
-export * from './user.controller';
+export * from './user.interface';
 export * from './user.service';
--- server/src/components/User/user.routes.ts
+++ server/src/components/User/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { register } from './user.controller';
 const usersRouter = Router();
 
 usersRouter.get('/');
 usersRouter.post('/login');
-usersRouter.post('/register');
+usersRouter.post('/register', register);
 usersRouter.post('/logout');
 usersRouter.patch('/:userId/update');
 usersRouter.patch('/:userId/password-reset');

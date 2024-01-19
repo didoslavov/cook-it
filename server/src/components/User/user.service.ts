7d14b50401b8aab3f2bb2bb93eee81af6733606b
didoslavov <didkoslawow@gmail.com>
2024-01-19 23:59:57 +0200

    find user service added

--- server/src/components/User/user.service.ts
+++ server/src/components/User/user.service.ts
@@ -3,3 +3,7 @@ import { User, UserInterface } from '../User';
 export const createUser = async (user: UserInterface): Promise<User> => {
     return await User.create(user);
 };
+
+export const findUser = async (email: string): Promise<UserInterface | undefined> => {
+    return await User.findOne({ where: { email } }).then((user): UserInterface | undefined => user?.toJSON());
+};

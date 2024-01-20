8820a4049611900aee9c78e2ac072351094d63f4
didoslavov <didkoslawow@gmail.com>
2024-01-21 00:23:31 +0200

    using the user validations

--- server/src/components/User/user.routes.ts
+++ server/src/components/User/user.routes.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { register, login, logout } from './auth.controller';
 import authMiddleware from '../Shared/middlewares/authMiddleware';
 import { getUsers, updateUser } from './user.controller';
+import { loginValidation, registerValidation } from './user.validation';
 const usersRouter = Router();
 
 usersRouter.get('/', getUsers);
-usersRouter.post('/login', login);
-usersRouter.post('/register', register);
+usersRouter.post('/login', loginValidation, login);
+usersRouter.post('/register', registerValidation, register);
 usersRouter.post('/logout', authMiddleware(), logout);
 usersRouter.patch('/:userId/update', updateUser);
 

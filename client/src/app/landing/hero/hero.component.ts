adaa67c9577e53e2882cd659d2892a49ef5d4431
didoslavov <didkoslawow@gmail.com>
2024-01-22 03:30:06 +0200

    hero section component added

---
+++ client/src/app/landing/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}

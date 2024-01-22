d05c26ebc7ff1bd5e4f99c178737e22574397037
didoslavov <didkoslawow@gmail.com>
2024-01-22 04:58:49 +0200

    added header component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingComponent],
+  imports: [CommonModule, RouterOutlet, LandingComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

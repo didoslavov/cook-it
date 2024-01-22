c51aaf7cfddda886809d0479fd28c7e78f94ca76
didoslavov <didkoslawow@gmail.com>
2024-01-22 17:57:26 +0200

    mobile navigation and media queries for responsive navigation menu

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  faCircleXmark = faCircleXmark;
+  faBars = faBars;
+  showMobileNav = false;
 
+  toggleMobileNav() {
+    this.showMobileNav = !this.showMobileNav;
+  }
+
+  closeMobileNav() {
+    this.showMobileNav = false;
+  }
 }

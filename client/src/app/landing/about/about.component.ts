727086d5ae11df3e89d39d1f633ae31297b0f70f
didoslavov <didkoslawow@gmail.com>
2024-01-24 01:11:05 +0200

    added about component

---
+++ client/src/app/landing/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+})
+export class AboutComponent {
+  faCircle = faCircle;
+}
--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { HowComponent } from './how/how.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, HowComponent],
+  imports: [HeroComponent, HowComponent, AboutComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })

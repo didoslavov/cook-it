d3149d3d58a1b50de3e58082ffb78a4e3ae45309
didoslavov <didkoslawow@gmail.com>
2024-01-26 04:25:29 +0200

    added footer to landing component

--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { HowComponent } from './how/how.component';
 import { AboutComponent } from './about/about.component';
+import { FooterComponent } from '../core/footer/footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent, HowComponent, AboutComponent],
+  imports: [HeroComponent, HowComponent, AboutComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })

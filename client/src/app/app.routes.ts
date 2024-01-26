bf5af3b1b70302346149c65b265b42d878c2824b
didoslavov <didkoslawow@gmail.com>
2024-01-26 04:24:59 +0200

    added routes to navigation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent,
+  },
+];
--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })

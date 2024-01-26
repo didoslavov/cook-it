70208f2edbf6adb87ca27cd6e66b30756d2b93de
didoslavov <didkoslawow@gmail.com>
2024-01-26 04:23:46 +0200

    removing footer from app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    LandingComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

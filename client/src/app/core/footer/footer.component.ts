9328027f7f2521d572bbf815075754d8372065cf
didoslavov <didkoslawow@gmail.com>
2024-01-27 03:49:54 +0200

    changed href to routerLink in footers links

--- client/src/app/core/footer/footer.component.ts
+++ client/src/app/core/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })

1d73ce79db05c96a260758cfb9c8cab34300fede
didoslavov <didkoslawow@gmail.com>
2024-01-28 03:12:07 +0200

    authentication actions for ngrx store added

---
+++ client/src/app/store/auth/auth.actions.ts
@@ -0,0 +1,22 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Credentials, User, UserData } from './user.model';
+
+export const AuthPageActions = createActionGroup({
+  source: 'Auth Page',
+  events: {
+    registerClicked: emptyProps(),
+    loginClicked: emptyProps(),
+  },
+});
+
+export const AuthApiActions = createActionGroup({
+  source: 'Auth API',
+  events: {
+    registerUser: props<{ userData: UserData }>(),
+    registrationSuccess: props<{ user: User }>(),
+    registrationFailure: props<{ error: string }>(),
+    loginUser: props<{ credentials: Credentials }>(),
+    loginSuccess: props<{ user: User }>(),
+    loginFailure: props<{ error: string }>(),
+  },
+});

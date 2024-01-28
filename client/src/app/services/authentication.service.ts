03266cd39d283f8d098183ee0fa293d23af7b93b
didoslavov <didkoslawow@gmail.com>
2024-01-28 14:55:24 +0200

    authentication service added

---
+++ client/src/app/services/authentication.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Credentials, User, UserData } from '../store/auth/user.model';
+import { Observable, catchError, of, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthApiActions } from '../store/auth/auth.actions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  constructor(
+    private cookieService: CookieService,
+    private store: Store,
+    private http: HttpClient
+  ) {}
+
+  extractTokenFromCookie(): string | null {
+    const token = this.cookieService.get('auth');
+    return token || null;
+  }
+
+  register(userData: UserData): Observable<User> {
+    return this.http
+      .post<User>('/auth/register', userData)
+      .pipe(
+        tap((user) =>
+          this.store.dispatch(AuthApiActions.registrationSuccess({ user }))
+        )
+      );
+  }
+
+  login(credentials: Credentials): Observable<User> {
+    return this.http
+      .post<User>('/auth/login', credentials)
+      .pipe(
+        tap((user) =>
+          this.store.dispatch(AuthApiActions.loginSuccess({ user }))
+        )
+      );
+  }
+
+  logout(): Observable<Object> {
+    return this.http.post('/auth/logout', null);
+  }
+}

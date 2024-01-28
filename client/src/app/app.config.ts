d91b9814313ec677a87005656fb71dba8bd249a3
didoslavov <didkoslawow@gmail.com>
2024-01-28 03:11:23 +0200

    added ngrx store and effects to the providers

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withViewTransitions()),
-    provideAnimations(),
-  ],
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), provideStore(), provideEffects()],
 };

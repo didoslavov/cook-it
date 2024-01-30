f9c55a72bf2810b027b1e4b00f40acc8d1477a51
didoslavov <didkoslawow@gmail.com>
2024-01-30 18:49:56 +0200

    animated arrow added in learn more button

--- client/src/app/landing/hero/hero.component.ts
+++ client/src/app/landing/hero/hero.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  faArrowDown = faArrowDown;
 }

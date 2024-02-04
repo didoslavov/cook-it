689c05a9feeefebc84c987eaa3123229ba5112c6
didoslavov <didkoslawow@gmail.com>
2024-02-05 00:30:22 +0200

    getting all recipes and pass it to carousel

--- client/src/app/recipes/recipes.component.ts
+++ client/src/app/recipes/recipes.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CarouselComponent } from '../shared/carousel/carousel.component';
+import { RecipeData } from './recipe.model';
+import { RecipeService } from '../services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
@@ -8,4 +11,26 @@ import { CarouselComponent } from '../shared/carousel/carousel.component';
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss',
 })
-export class RecipesComponent {}
+export class RecipesComponent implements OnInit, OnDestroy {
+  recipes: RecipeData[] = [];
+  private subscription!: Subscription;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.recipeService.getAllRecipes().subscribe(
+      (recipes: RecipeData[]) => {
+        this.recipes = recipes;
+      },
+      (error) => {
+        console.error('Error fetching recipes:', error);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}

dd90a6fa3f1ca3b7718c1ab5faa812091ae97015
didoslavov <didkoslawow@gmail.com>
2024-02-05 00:07:28 +0200

    service to create product-recipe relation

---
+++ server/src/components/Shared/Relationships/ProductRecipe/productRecipe.service.ts
@@ -0,0 +1,13 @@
+import { Ingredient } from '../../../Product';
+import ProductRecipe from './ProductRecipe.model';
+
+export const createProductRecipe = async (recipeId: string, ingredients: Ingredient[]) => {
+    await ProductRecipe.bulkCreate(
+        ingredients.map((i) => ({
+            recipeId,
+            productId: i.id,
+            quantity: Number(i.quantity),
+            quantity_unit: i.unit,
+        }))
+    );
+};

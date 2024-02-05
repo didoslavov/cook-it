9de3ee9afcfd10727e9b1322624fa11fd8ae1ecf
didoslavov <didkoslawow@gmail.com>
2024-02-05 19:01:19 +0200

    added link to the header for navigating to details later

--- client/src/app/shared/recipe-card/recipe-card.component.ts
+++ client/src/app/shared/recipe-card/recipe-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faClock, faInfo } from '@fortawesome/free-solid-svg-icons';
 import { RecipeData } from '../../recipes/recipe.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterModule],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss',
 })

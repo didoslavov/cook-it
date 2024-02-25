0a4a583f1ff6b66a0b17803694140e621efb2e19
didoslavov <didkoslawow@gmail.com>
2024-02-25 19:46:20 +0200

    fixed grid container and items width

--- server/src/components/Shared/Relationships/ProductRecipe/productRecipe.service.ts
+++ server/src/components/Shared/Relationships/ProductRecipe/productRecipe.service.ts
@@ -1,4 +1,4 @@
-import { Ingredient, Product } from '../../../Product';
+import { Ingredient, Product, ProductInterface } from '../../../Product';
 import ProductRecipe from './ProductRecipe.model';
 
 export const createProductRecipe = async (recipeId: string, ingredients: Ingredient[]) => {
@@ -14,52 +14,53 @@ export const createProductRecipe = async (recipeId: string, ingredients: Ingredi
 
 export const updateIngredientRecipe = async (
     recipeId: string,
-    ingredientId: string,
-    name: string,
+    oldIngredientId: string,
+    newIngredientName: string,
     quantity: string | undefined,
     unit: string | undefined
 ): Promise<void> => {
     try {
-        const existingProduct = await Product.findByPk(ingredientId);
+        let existingProduct = await Product.findOne({ where: { name: newIngredientName } });
 
-        if (existingProduct) {
-            if (existingProduct.name !== name) {
-                const [newProduct] = await Product.findOrCreate({ where: { name } });
+        if (!existingProduct) {
+            existingProduct = await Product.create({ name: newIngredientName, ProductRecipe: { quantity, unit } });
+        }
 
-                await ProductRecipe.destroy({ where: { recipeId, productId: existingProduct.id } });
+        const existingProductRecipe = await ProductRecipe.findOne({
+            where: { recipeId, productId: oldIngredientId },
+        });
 
-                if (quantity !== undefined && unit !== undefined) {
-                    await ProductRecipe.create({
-                        recipeId,
-                        productId: newProduct.id,
-                        quantity,
-                        unit,
-                    });
-                }
-            } else {
-                const updateData: any = { name };
-                if (quantity !== undefined && unit !== undefined) {
-                    updateData.ProductRecipe = { quantity, unit };
-                }
-                await existingProduct.update(updateData);
+        const [productRecipe, created] = await ProductRecipe.findOrCreate({
+            where: { recipeId, productId: existingProduct.id },
+            defaults: { recipeId, productId: existingProduct.id },
+        });
 
-                if (quantity !== undefined && unit !== undefined) {
-                    await ProductRecipe.update({ quantity, unit }, { where: { recipeId, productId: existingProduct.id } });
-                }
-            }
+        if (!created && existingProductRecipe) {
+            productRecipe.quantity = quantity !== undefined ? quantity : existingProductRecipe.quantity;
+            productRecipe.unit = unit !== undefined ? unit : existingProductRecipe.unit;
+
+            await productRecipe.save();
         } else {
-            const newProduct = await Product.create({ name, ProductRecipe: { quantity, unit } });
-            if (quantity !== undefined && unit !== undefined) {
-                await ProductRecipe.create({
-                    recipeId,
-                    productId: newProduct.id,
-                    quantity,
-                    unit,
-                });
-            }
+            productRecipe.quantity = quantity;
+            productRecipe.unit = unit;
+
+            await productRecipe.save();
+        }
+
+        if (oldIngredientId !== existingProduct.id) {
+            await ProductRecipe.destroy({ where: { recipeId, productId: oldIngredientId } });
         }
     } catch (error) {
         console.error('Error updating ingredient:', error);
+        throw error;
+    }
+};
+
+export const removeIngredientFromRecipe = async (recipeId: string, ingredientId: string | undefined): Promise<void> => {
+    try {
+        await ProductRecipe.destroy({ where: { recipeId, productId: ingredientId } });
+    } catch (error) {
+        console.error('Error removing ingredient from recipe:', error);
         throw error;
     }
 };

b9a72ed5fb3bc5fb2a7219ba53cf73260f3f75c7
didoslavov <didkoslawow@gmail.com>
2024-02-29 23:50:02 +0200

    conditionally apply background class to resolve 2 layers background

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,21 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterModule,
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent implements OnInit {
   declare userId: string | null;
+  isRecipesRoute: boolean = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.userId = params['userId'];
     });
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isRecipesRoute =
+          this.route.snapshot.firstChild?.routeConfig?.path === 'recipes';
+      });
+  }
+
+  setRecipesLoaded(status: boolean): void {
+    this.isRecipesRoute = status;
   }
 }

2f4198ceb1baca8106b300d4536f7ed9f5e77232
didoslavov <didkoslawow@gmail.com>
2024-02-29 23:02:52 +0200

    profile component added

--- client/src/app/user/profile/profile.component.ts
+++ client/src/app/user/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,4 +8,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  declare userId: string | null;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.userId = params['userId'];
+    });
+  }
+}

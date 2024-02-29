330a882bcf004a8e5879e189c354c3d146ff613a
didoslavov <didkoslawow@gmail.com>
2024-02-29 03:45:40 +0200

    profile component added

---
+++ client/src/app/user/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss',
+})
+export class ProfileComponent {}

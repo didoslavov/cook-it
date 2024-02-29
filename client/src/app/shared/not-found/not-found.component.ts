98290aa456128eaa811fa1333492a5216e18915b
didoslavov <didkoslawow@gmail.com>
2024-02-29 23:05:01 +0200

    changed href to routerLink

--- client/src/app/shared/not-found/not-found.component.ts
+++ client/src/app/shared/not-found/not-found.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent {}

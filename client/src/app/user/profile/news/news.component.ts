f53251a48b4a3330dee7c1c822dd901c74430782
didoslavov <didkoslawow@gmail.com>
2024-03-04 18:36:04 +0200

    implemented url params to be updated accordingly
    The component uses technique to get params from the url and make the correct request on initializing

--- client/src/app/user/profile/news/news.component.ts
+++ client/src/app/user/profile/news/news.component.ts
@@ -5,11 +5,9 @@ import { RecipeService } from '../../../services/recipe.service';
 import { NewsData } from '../../../recipes/recipe.model';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FontawesomeObject } from '@fortawesome/fontawesome-svg-core';
-import {
-  IconDefinition,
-  faCalendarAlt,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-news',
@@ -28,15 +26,35 @@ export class NewsComponent implements OnInit {
   visibleNews: NewsData[] = [];
   page = 1;
   pageSize = 20;
+  initialScrollPosition = 0;
+  previousScrollPosition = 0;
 
+  private declare navigationEndSubscription: Subscription;
   declare faCalendar;
 
-  constructor(private newsService: RecipeService) {
+  constructor(
+    private newsService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.faCalendar = faCalendarAlt;
   }
 
   ngOnInit() {
-    this.loadNews();
+    this.route.queryParams.subscribe((params) => {
+      this.page = parseInt(params['page'] || '1', 10);
+      this.loadNews();
+    });
+
+    this.navigationEndSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.loadNews();
+      });
+
+    if (!this.route.snapshot.queryParams['page']) {
+      this.router.navigate([], { queryParams: { page: 1 } });
+    }
   }
 
   loadNews() {
@@ -49,12 +67,17 @@ export class NewsComponent implements OnInit {
   }
 
   loadMoreNews() {
-    this.page++;
+    if (this.page !== 1) {
+      this.page++;
+    }
+
     this.newsService
       .getLatestNews(this.page, this.pageSize)
       .subscribe((news) => {
         const nextBatch = news.articles;
-        this.visibleNews = this.visibleNews.concat(nextBatch);
+        this.visibleNews = [...this.visibleNews, ...nextBatch];
+
+        this.router.navigate([], { queryParams: { page: this.page } });
       });
   }
 

00dfd2449be77b74aef37088fc9b29fbd332e159
didoslavov <didkoslawow@gmail.com>
2024-03-06 05:16:14 +0200

    refactored and simplified the carousel component to implement pagination

--- client/src/app/shared/carousel/carousel.component.ts
+++ client/src/app/shared/carousel/carousel.component.ts
@@ -6,21 +6,18 @@ import {
   OnChanges,
 } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {
-  faAngleDown,
-  faAngleLeft,
-  faAngleRight,
-  faAngleUp,
-} from '@fortawesome/free-solid-svg-icons';
-import { RecipeData } from '../../recipes/recipe.model';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { Recipe, RecipeData } from '../../recipes/recipe.model';
 import { RecipeService } from '../../services/recipe.service';
 import { RecipeCardComponent } from '../../recipes/recipe-card/recipe-card.component';
 import { Store, select } from '@ngrx/store';
 import { getUserData } from '../../store/auth/auth.selectors';
 import { User } from '../../store/auth/user.model';
-import { ActivatedRoute, Router, UrlTree } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, UrlTree } from '@angular/router';
 import { HttpParams } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
+import { FontawesomeObject } from '@fortawesome/fontawesome-svg-core';
 
 type RecipeServiceMethod =
   | 'getRecipes'
@@ -30,146 +27,93 @@ type RecipeServiceMethod =
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [RecipeCardComponent, FontAwesomeModule, CommonModule],
+  imports: [RecipeCardComponent, FontAwesomeModule, CommonModule, RouterModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
-export class CarouselComponent implements OnInit, OnChanges {
+export class CarouselComponent implements OnInit {
   @Input() carouselType: 'all' | 'user' | 'search' = 'all';
   @Input() title: string = '';
   @Input() ingredients: string[] = [];
 
-  recipes: RecipeData[] = [];
-  faArrowLeft = faAngleLeft;
-  faArrowRight = faAngleRight;
-
   declare user: User | null;
+  declare faArrowLeft;
+  declare faArrowRight;
+
+  recipes: Recipe[] = [];
+  pagination: number[] = [];
 
   currentOffset = 0;
   limit = 4;
 
-  private currentUrlTree: UrlTree;
-  private fetchMethod: RecipeServiceMethod = 'getRecipes';
-
   constructor(
     private recipeService: RecipeService,
     private store: Store,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ) {
-    this.currentUrlTree = this.router.createUrlTree(['/recipes']);
+    this.faArrowLeft = faAngleLeft;
+    this.faArrowRight = faAngleRight;
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['ingredients']) {
-      this.fetchRecipes(this.fetchMethod, 0, this.limit);
-    }
-  }
+  private unsubscribe$ = new Subject<void>();
 
   ngOnInit(): void {
-    this.store.pipe(select(getUserData)).subscribe((user: any) => {
-      this.user = user?.user;
-    });
-
-    this.route.queryParamMap.subscribe((params) => {
-      this.currentOffset = Number(params.get('offset')) || 0;
-    });
-
-    if (this.carouselType === 'user') {
-      this.fetchMethod = 'getUserRecipes';
-      const userId = this.user ? this.user.id : '';
-      this.currentUrlTree = this.router.createUrlTree(
-        [`/profile/${userId}/recipes`],
-        {
-          queryParams: {
-            offset: this.currentOffset.toString(),
-            limit: this.limit.toString(),
-          },
-        }
-      );
-    } else if (this.carouselType === 'search' && this.ingredients.length > 0) {
-      this.fetchMethod = 'searchRecipesByIngredients';
-      const ingredientsString = this.ingredients.join(',');
-      this.currentUrlTree = this.router.createUrlTree(
-        [`/profile/${this.user?.id}/search`],
-        {
-          queryParams: {
-            ingredients: ingredientsString,
-            offset: this.currentOffset.toString(),
-            limit: this.limit.toString(),
-          },
-        }
-      );
-    } else {
-      this.fetchMethod = 'getRecipes';
-      this.currentUrlTree = this.router.createUrlTree(['/recipes'], {
-        queryParams: {
-          offset: this.currentOffset.toString(),
-          limit: this.limit.toString(),
+    this.route.queryParams
+      .pipe(
+        switchMap((params) => {
+          const offset = parseInt(params['offset'] || '0');
+          return this.fetchRecipes(offset);
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe({
+        next: (recipeData: RecipeData) => {
+          this.recipes = recipeData.recipes;
+          this.pagination = new Array(Math.ceil(recipeData.count / this.limit))
+            .fill(0)
+            .map((_, i) => i + 1);
+        },
+        error: () => {
+          this.recipes = [];
         },
       });
-    }
 
-    this.router.navigateByUrl(this.currentUrlTree);
-    this.fetchRecipes(this.fetchMethod);
+    this.store
+      .pipe(select(getUserData), takeUntil(this.unsubscribe$))
+      .subscribe((user: any) => {
+        this.user = user?.user;
+      });
   }
 
-  private fetchRecipes(
-    method: RecipeServiceMethod,
-    offset: number = this.currentOffset,
-    limit: number = this.limit
-  ): void {
-    let params = new HttpParams()
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  private fetchRecipes(offset: number): Observable<RecipeData> {
+    let queryParams = new HttpParams()
       .set('offset', offset.toString())
-      .set('limit', limit.toString());
+      .set('limit', this.limit.toString());
 
     if (this.carouselType === 'search' && this.ingredients.length > 0) {
-      const ingredientsString = this.ingredients.join(',');
-      params = params.set('ingredients', ingredientsString);
+      queryParams = queryParams.set('ingredients', this.ingredients.join(','));
     }
 
-    this.recipeService[method](params).subscribe({
-      next: (recipes: RecipeData[]) => {
-        this.recipes = recipes;
-      },
-      error: () => {
-        this.recipes = [];
-      },
-    });
+    const method = this.getMethod();
+
+    return this.recipeService[method](queryParams);
   }
 
-  showNextRecipes(): void {
-    if (!this.recipes.length) return;
-
-    this.currentOffset += this.limit;
-
-    this.navigateToRecipes();
-  }
-
-  showPreviousRecipes(): void {
-    this.currentOffset -= this.limit;
-
-    if (this.currentOffset < 0) {
-      this.currentOffset = 0;
-      return;
-    }
-
-    this.navigateToRecipes();
-  }
-
-  private navigateToRecipes(): void {
-    let url = '';
-
-    if (this.carouselType === 'search') {
-      const ingredientsString = this.ingredients.join(',');
-      url = `/profile/${this.user?.id}/search?ingredients=${ingredientsString}&offset=${this.currentOffset}&limit=${this.limit}`;
-    } else if (this.carouselType === 'user') {
-      url = `/profile/${this.user?.id}/recipes?offset=${this.currentOffset}&limit=${this.limit}`;
+  private getMethod():
+    | 'getRecipes'
+    | 'getUserRecipes'
+    | 'searchRecipesByIngredients' {
+    if (this.carouselType === 'user') {
+      return 'getUserRecipes';
+    } else if (this.carouselType === 'search' && this.ingredients.length > 0) {
+      return 'searchRecipesByIngredients';
     } else {
-      url = `/recipes?offset=${this.currentOffset}&limit=${this.limit}`;
+      return 'getRecipes';
     }
-
-    this.router.navigateByUrl(url);
-    this.fetchRecipes(this.fetchMethod, this.currentOffset);
   }
 }

f27fcd17b102d55d0258806d6ee3d5e5a5d9e8e6
didoslavov <didkoslawow@gmail.com>
2024-03-10 12:29:56 +0200

    added first name validations

---
+++ client/src/app/validators/firstName.validators.ts
@@ -0,0 +1,8 @@
+import { Validators } from '@angular/forms';
+
+export const firstNameValidators = {
+  required: Validators.required,
+  minLength: Validators.minLength(4),
+  maxLength: Validators.maxLength(50),
+  charSet: Validators.pattern('[a-zA-Z ]*'),
+};

39d1b8135d3be889242f5773be3ecedf2ee2b928
didoslavov <didkoslawow@gmail.com>
2024-03-12 15:45:35 +0200

    display more descriptive error messages

--- client/src/app/shared/error/error.component.ts
+++ client/src/app/shared/error/error.component.ts
@@ -2,23 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ErrorService } from '../../services/error.service';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss',
 })
 export class ErrorComponent implements OnInit {
-  errors: string[] = [];
   declare faExclamation;
+
+  errors: { [inputName: string]: {} } = {};
 
   constructor(private errorService: ErrorService) {
     this.faExclamation = faExclamationCircle;
   }
 
-  ngOnInit() {
-    this.errorService.errors$.subscribe((errors) => (this.errors = errors));
+  ngOnInit(): void {
+    this.errorService.errors$.subscribe((errors) => {
+      this.errors = errors;
+    });
   }
 }

27c65dce26d9a054649fdb89b517d07ed72e65ca
didoslavov <didkoslawow@gmail.com>
2024-03-13 01:55:45 +0200

    error handling for search input

--- client/src/app/user/profile/profile-search/profile-search.component.ts
+++ client/src/app/user/profile/profile-search/profile-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -10,23 +10,41 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTurnUp } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../../../recipes/recipe.model';
+import { ErrorService } from '../../../services/error.service';
+import { Subject, Subscription, takeUntil } from 'rxjs';
+import { ErrorComponent } from '../../../shared/error/error.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile-home',
   standalone: true,
-  imports: [ReactiveFormsModule, CarouselComponent, FontAwesomeModule],
+  imports: [
+    ReactiveFormsModule,
+    CarouselComponent,
+    FontAwesomeModule,
+    ErrorComponent,
+    CommonModule,
+  ],
   templateUrl: './profile-search.component.html',
   styleUrl: './profile-search.component.scss',
 })
-export class ProfileSearchComponent implements OnInit {
+export class ProfileSearchComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
   recipes: Recipe[] | null = [];
   ingredients: string[] = [];
-  pagination: number = 0;
+  pagination = 0;
+  hasErrors = false;
+
+  private errorsSubscription: Subscription | undefined;
+  private destroy$: Subject<void> = new Subject<void>();
 
   declare faArrowUp;
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private errorService: ErrorService
+  ) {
     this.faArrowUp = faTurnUp;
     this.searchForm = this.formBuilder.group({
       searchQuery: ['', Validators.required],
@@ -38,25 +56,57 @@ export class ProfileSearchComponent implements OnInit {
       const ingredientsParam = params['ingredients'];
       if (ingredientsParam) {
         try {
-          //? I need to further investigate this edge case.
-          //? When navigating in carouselType = 'search',
-          //? only with arrow(forward and back) there's an error,
-          //? ingredientsParams is string[].
           this.ingredients = ingredientsParam.split(', ');
         } catch (e) {}
       } else {
         this.ingredients = [];
       }
     });
+
+    this.errorsSubscription = this.errorService.errors$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((errors) => {
+        this.hasErrors = Object.keys(errors).some(
+          (inputName) => Object.keys(errors[inputName]).length > 0
+        );
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.errorsSubscription) {
+      Object.keys(this.searchForm.controls).forEach((i) =>
+        this.errorService.clearErrors(i)
+      );
+      this.errorsSubscription.unsubscribe();
+    }
   }
 
   onSubmit(): void {
     if (this.searchForm.valid) {
+      this.hasErrors = false;
       const searchQuery = this.searchForm.value.searchQuery;
 
       this.ingredients = searchQuery
         .split(', ')
         .map((ingredient: string) => ingredient.trim());
+    } else {
+      Object.keys(this.searchForm.controls).forEach((input) => {
+        const control = this.searchForm.get(input);
+
+        if (control && control.errors) {
+          const currentErrors = control.errors as { [errorKey: string]: any };
+
+          for (const errorKey in currentErrors) {
+            if (currentErrors.hasOwnProperty(errorKey)) {
+              this.errorService.setErrors(input, {
+                [errorKey]: currentErrors[errorKey],
+              });
+            }
+          }
+        } else {
+          this.errorService.clearErrors(input);
+        }
+      });
     }
   }
 }

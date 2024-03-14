de2b2d1213634efd77790a7b0e6d8b9198333dc9
didoslavov <didkoslawow@gmail.com>
2024-03-15 00:03:18 +0200

    set notification in route guards

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -2,10 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { getUserData } from '../store/auth/auth.selectors';
+import { NotificationService } from '../services/notification.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const store = inject(Store);
+  const notificationService = inject(NotificationService);
 
   const userId = route.params['userId'];
   let user;
@@ -14,6 +16,10 @@ export const authGuard: CanActivateFn = (route, state) => {
     user = user?.user;
 
     if (!user) {
+      notificationService.setNotification({
+        message: "You don't have access to this recource. Please sign in.",
+        type: 'error',
+      });
       router.navigate(['/auth/login']);
     }
 
--- client/src/app/guards/guest.guard.ts
+++ client/src/app/guards/guest.guard.ts
@@ -2,14 +2,21 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { getUserData } from '../store/auth/auth.selectors';
+import { NotificationService } from '../services/notification.service';
 
 export const guestGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const store = inject(Store);
+  const notificationService = inject(NotificationService);
+
   let user;
 
   store.pipe(select(getUserData)).subscribe((userData: any) => {
     if (userData) {
+      notificationService.setNotification({
+        message: "You're already signed.",
+        type: 'error',
+      });
       router.navigate(['']);
     }
   });

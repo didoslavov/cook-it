d6894ab3900ca566c73d76ea66fddbf57acca165
didoslavov <didkoslawow@gmail.com>
2024-03-20 03:54:19 +0200

    fix bug where notification for log out is not set

--- client/src/app/core/header/user-menu/user-menu/user-menu.component.ts
+++ client/src/app/core/header/user-menu/user-menu/user-menu.component.ts
@@ -6,14 +6,13 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
-import { AuthApiActions } from '../../../../store/auth/auth.actions';
 import { Store, select } from '@ngrx/store';
 import { getUserData } from '../../../../store/auth/auth.selectors';
-import { NotificationService } from '../../../../services/notification.service';
 import { Subscription } from 'rxjs';
+import { NotificationService } from '../../../../services/notification.service';
 
 @Component({
   selector: 'app-user-menu',
@@ -32,12 +31,17 @@ export class UserMenuComponent implements OnInit, OnDestroy {
   declare faUser;
   declare faLogout;
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private notificationService: NotificationService
+  ) {
     this.faUser = faUser;
     this.faLogout = faRightFromBracket;
   }
 
   ngOnInit(): void {
+    console.log(this.logout);
+
     this.userDataSubscription = this.store
       .pipe(select(getUserData))
       .subscribe((user: any) => {
@@ -45,7 +49,16 @@ export class UserMenuComponent implements OnInit, OnDestroy {
       });
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.userDataSubscription.unsubscribe();
+  }
+
+  onLogout() {
+    this.notificationService.setNotification({
+      message: 'Logged out successfully',
+      type: 'success',
+    });
+
+    this.logout();
   }
 }

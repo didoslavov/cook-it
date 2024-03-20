eb9b8ee2226e64486e1d803ca75aab7815f02ec7
didoslavov <didkoslawow@gmail.com>
2024-03-20 03:34:55 +0200

    add notificaitons for success and error

--- client/src/app/shared/delete/delete.component.ts
+++ client/src/app/shared/delete/delete.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
 import { RecipeService } from '../../services/recipe.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-delete',
@@ -17,7 +18,8 @@ export class DeleteComponent implements OnInit {
   constructor(
     private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private notificationService: NotificationService
   ) {}
 
   ngOnInit() {
@@ -27,8 +29,22 @@ export class DeleteComponent implements OnInit {
   }
 
   onDelete() {
-    this.recipeService.deleteRecipe(this.recipeId).subscribe(() => {
-      this.router.navigate(['/recipes']);
+    this.recipeService.deleteRecipe(this.recipeId).subscribe({
+      next: () => {
+        this.notificationService.setNotification({
+          message: 'Recipe deleted successfully',
+          type: 'success',
+        });
+
+        this.router.navigate(['/recipes']);
+      },
+      error: (error) => {
+        this.notificationService.setNotification({
+          message: `There was a problem deleting the recipe
+          ${error.message}`,
+          type: 'error',
+        });
+      },
     });
   }
 
